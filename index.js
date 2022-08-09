2bb17d5e0a2c693167d2f55ec929220b5cc5eb4b
vuongcdt <vuongcdt@gmail.com>
2022-08-09 16:41:34 +0700

    deploy heroku

--- index.js
+++ index.js
@@ -4,32 +4,31 @@ const { connectToDb } = require("./database");
 const { editData } = require("./editData");
 const router = require("./routers");
 
-console.log("process.env.PORT:", process.env.PORT, process.env.URI_MONGODB);
-const port = process.env.PORT;
-const URI = "mongodb+srv://admin:admin123@cluster0.yy8pv.mongodb.net/test";
+console.log('process.env.PORT:',process.env.PORT,process.env.URI_MONGODB)
+const port = process.env.PORT ;
+
 const app = express();
 
 app.use(express.json());
 
-app.use("/api", router);
+app.use('/api',router);
 
 app.get("/", async (req, res) => {
    res.send("sever is runing");
 });
 
 // console.log(`  *** editData()`, editData())
-
+ 
 // app.use((err, req, res, next) => {
 //    if (err) {
 //       console.log("ERROR", err);
 //       res.send("error  " + err);
 //    } else {
 //       next();
-//    }
+//    } 
 // });
 
-connectToDb(URI);
-// connectToDb(process.env.URI_MONGODB);
+connectToDb(process.env.URI_MONGODB);
 
 app.listen(port, () => {
    console.log(`Sever is runing at port ${port}`);
