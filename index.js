e777f5df807bc77da934daeca01ef5376e69b9d0
vuongcdt <vuongcdt@gmail.com>
2022-08-09 21:01:44 +0700

    deploy vercel

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
-require("dotenv").config();
 const express = require("express");
 const { connectToDb } = require("./database");
 const { editData } = require("./editData");
 const router = require("./routers");
 
-console.log('process.env.PORT:',process.env.PORT,process.env.MONGODB_URI)
-const port = process.env.PORT ;
+console.log("process.env.PORT:", process.env.PORT, process.env.MONGODB_URI);
+const port = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(express.json());
- 
-app.use('/api',router);
+
+app.use("/api", router);
 
 app.get("/", async (req, res) => {
    res.send("sever is runing");
 });
 
 // console.log(`  *** editData()`, editData())
- 
-// app.use((err, req, res, next) => { 
+
+// app.use((err, req, res, next) => {
 //    if (err) {
 //       console.log("ERROR", err);
 //       res.send("error  " + err);
 //    } else {
 //       next();
-//    } 
+//    }
 // });
 
 connectToDb(process.env.MONGODB_URI);
