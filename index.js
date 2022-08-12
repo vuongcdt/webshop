5fc48b8c44ae4275ef6e0ab51a1423d69674fa0c
vuongcdt <vuongcdt@gmail.com>
2022-08-12 17:57:53 +0700

    fix discount  filter sidebar deploy

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use("/api", router);
 
 app.get("/", (req, res) => {
    wakeUp();
-   res.send("sever is runing: "+ process.env.MONGODB_URI);
+   res.send("Sever is runing: "+ process.env.MONGODB_URI);
 });
 
 // console.log(`  *** editData()`, editData())
