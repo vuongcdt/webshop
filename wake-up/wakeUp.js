37c4a141bc4fea0386ed0cea612d002a0ce0bc24
vuongcdt <vuongcdt@gmail.com>
2022-08-12 17:39:30 +0700

    fix rating price filter sidebar deploy

--- wake-up/wakeUp.js
+++ wake-up/wakeUp.js
@@ -5,7 +5,7 @@ const wakeUp = () => {
    fetchMyWeb();
    timeId = setInterval(() => {
       fetchMyWeb();
-   }, 3 * 60 * 1000);
+   }, 4 * 60 * 1000);
 };
 const fetchMyWeb = async () => {
    try {
